f76b3b234cbc699d391b63e53facabe328deddbe
parul-jain5102 <63896325+parul-jain5102@users.noreply.github.com>
2021-06-25 13:17:05 +0530

    Delete without head pointer (function)

---
+++ GFGLinkedList3.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *node)
+    {
+       // Your code here
+       node->data=node->next->data;
+       node->next=node->next->next;
+    }
+
+};
