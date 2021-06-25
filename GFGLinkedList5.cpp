03a3d78be723b7bd82cad46d103348d7a5d82cb0
parul-jain5102 <63896325+parul-jain5102@users.noreply.github.com>
2021-06-25 13:19:06 +0530

    Merge two sorted linked list (function)

---
+++ GFGLinkedList5.cpp
@@ -0,0 +1,27 @@
+struct Node* SortedMerge(struct Node* a, struct Node* b)
+{   struct Node dummy;
+ 
+    struct Node* tail = &dummy;
+ 
+    dummy.next = NULL;
+    while (1)
+    {
+        if (a == NULL)
+        {
+            tail->next = b;
+            break;
+        }
+        else if (b == NULL)
+        {
+            tail->next = a;
+            break;
+        }
+        if (a->data <= b->data)
+            MoveNode(&(tail->next), &a);
+        else
+            MoveNode(&(tail->next), &b);
+ 
+        tail = tail->next;
+    }
+    return(dummy.next);
+}
