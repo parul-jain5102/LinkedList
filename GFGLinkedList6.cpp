ea7ee64dc3c52ed41928e4e972334b8c99daa0b3
parul-jain5102 <63896325+parul-jain5102@users.noreply.github.com>
2021-06-25 13:20:08 +0530

    Reverse a linked list in groups of given size

---
+++ GFGLinkedList6.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    {
+    if (!head)
+        return NULL;
+    struct node* current = head;
+    struct node* next = NULL;
+    struct node* prev = NULL;
+    int count = 0;
+    while (current != NULL && count < k) 
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        count++;
+    }
+ 
+    if (next != NULL)
+        head->next = reverse(next, k);
+    return prev;
+    }
+};
